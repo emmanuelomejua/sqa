90002c44841a2474a19781959e66e452c9e4e945
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-12-24 22:32:41 +0100

    M

--- src/comps/navbar/navbar.tsx
+++ src/comps/navbar/navbar.tsx
@@ -4,7 +4,17 @@ const navbar = () => {
   return (
     <nav className='navbar'>
       <section className='navbar-cont'>
-        
+        <div className="navbar-left">
+          <img src='/logo.webp' alt='logo'/>
+        </div>
+
+        <div className="navbar-right">
+          <h2>SQA CHECKER</h2>
+          <div className="">
+            <img src='/person.jpg' alt='person' height='50px' className='n-img'/>
+            <span>Okojie Michael</span>
+          </div>
+        </div>
       </section>
     </nav>
   )
